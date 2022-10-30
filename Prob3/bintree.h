fde0efed13e827a322041f8d932fcf1e47754647
BobdaFett <lvas463@gmail.com>
2022-10-30 00:31:44 -0400

    Added and reformatted binary search files for Problem 3.

---
+++ Prob3/bintree.h
@@ -0,0 +1,16 @@
+#pragma once;
+#include <iostream>
+#include <string>
+#include "treeNode.h"
+using namespace std;
+
+class BinarySearchTree {
+public:
+   BinarySearchTree();
+   void insert(string data);
+   void erase(string data);
+   int count(string data) const;
+   void print() const;
+private:
+   TreeNode* root;
+};
